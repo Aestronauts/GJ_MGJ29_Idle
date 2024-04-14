c63903a9d2ccaa26adda9f3017c240e5059f3433
Noah <noahm1216@gmail.com>
2024-04-13 19:39:30 -0700

    More UI items

--- Aestro_IdleGameJam/Assets/Scripts/Managers/GameMusicManager.cs
+++ Aestro_IdleGameJam/Assets/Scripts/Managers/GameMusicManager.cs
@@ -16,8 +16,24 @@ public class GameMusicManager : MonoBehaviour
     [Space]
     public AudioSource aSource_Music;
     public AudioMixer AMG_Master;
-    public int currentSongId = 0;
+    public int currentSongId = 0, nextSongId = 0;
     public List<AudioClip> clips_precombat, clips_incombat, clips_postcombat;
+    private GAMESTATE lastGameState;
+    private bool changingSongNow = false;
+
+    private void OnEnable()
+    {
+        lastGameState = refGameManager.GameState;
+        ChangeSong();
+    }
+
+    public void LateUpdate()
+    {
+        if (lastGameState != refGameManager.GameState)
+        {
+            ChangeSong();
+        }
+    }
 
 
     public void ChangeSong()
@@ -28,20 +44,27 @@ public class GameMusicManager : MonoBehaviour
         {
             case GAMESTATE.INCOMBAT:
                 songRange.y = Random.Range(songRange.x, clips_incombat.Count-1);
-                currentSongId = Random.Range(songRange.x, songRange.y);
+                nextSongId = Random.Range(songRange.x, songRange.y);
                 break;
             case GAMESTATE.POSTCOMBAT:
                 songRange.y = Random.Range(songRange.x, clips_postcombat.Count-1);
-                currentSongId = Random.Range(songRange.x, songRange.y);
+                nextSongId = Random.Range(songRange.x, songRange.y);
                 break;
             case GAMESTATE.PRECOMBAT:
                 songRange.y = Random.Range(songRange.x, clips_precombat.Count-1);
-                currentSongId = Random.Range(songRange.x, songRange.y);
+                nextSongId = Random.Range(songRange.x, songRange.y);
                 break;
             default:
                 break;
         }
 
+        if(nextSongId == currentSongId)
+        {
+            ChangeSong();
+            return;
+        }
+
+        StartCoroutine(FadeMusic());
     }// end of ChangeSong()
 
 
@@ -49,7 +72,19 @@ public class GameMusicManager : MonoBehaviour
     // function to fade the master sound
     private IEnumerator FadeMusic()
     {
-        yield return new WaitForSeconds(0);
+        print("Transitioning Music");
+
+        float currentVolume = 0;
+        AMG_Master.GetFloat("VolumeMaster", out currentVolume);
+
+        //loop to lower volume
+        for (float i = currentVolume; i >= -50; i--)
+        {
+            AMG_Master.SetFloat("VolumeMaster", i);
+            yield return new WaitForSeconds(0.05f);
+        }
+        //change song
+
 
 
     }// end of FadeMusic()
